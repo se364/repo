2083e319d94314e60b7792e7ba5899af4f6fa48d
Shah Emran <5613586+se364@users.noreply.github.com>
2022-01-31 19:21:52 -0800

    Add files via upload

--- calculator.test.js
+++ calculator.test.js
@@ -25,3 +25,59 @@ test("multiplying 2 and 8 should return 16", () => {
     // assert
     expect(result).toBe(16);
 });
+
+
+// matchers - truth & falsy 
+// matchers - Number 
+// matchers - string match
+// matchers - exception matcher
+
+test("equality matchers", () => {
+    expect(2*2).toBe(4);
+    expect(4-2).not.toBe(1);
+  })
+
+  test("truthy operators", () => {
+    var name="Software testing help"
+    var n = null
+    expect(n).toBeNull()
+    expect(name).not.toBeNull
+  
+    // name has a valid value
+    expect(name).toBeTruthy()
+  
+    //fail - as null is non success
+    expect(n).toBeTruthy()
+    
+    // pass - null treated as false or negative
+    expect(n).toBeFalsy()
+  
+    // 0 - treated as false
+    expect(0).toBeFalsy()
+  })
+
+  test("numeric operators", () => {
+ 
+    var num1 = 100;
+    var num2 = -20;
+    var num3 = 0;
+  
+    // greater than
+    expect(num1).toBeGreaterThan(10)
+  
+    // less than or equal
+    expect(num2).toBeLessThanOrEqual(0)
+  
+    // greater than or equal
+    expect(num3).toBeGreaterThanOrEqual(0)
+  })
+
+  test("string matchers",() => {
+    var string1 = "software testing help - a great resource for testers"
+  
+    // test for success match
+    expect(string1).toMatch(/test/);
+  
+    // test for failure match
+    expect(string1).not.toMatch(/abc/)
+  })
